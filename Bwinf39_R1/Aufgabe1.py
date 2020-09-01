b3c97826cc95490cd54c47c402a1d2aeb6498cb8
TPVPfaller <48595504+TPVPfaller@users.noreply.github.com>
2020-09-01 18:10:43 +0200

    finished Aufgabe 1

--- Bwinf39_R1/Aufgabe1.py
+++ Bwinf39_R1/Aufgabe1.py
@@ -1,11 +1,11 @@
 from collections import defaultdict
 
+
 def read(example):
     file = open("raetsel" + example + ".txt", "r")
     words = ""
     spaces = ""
     for line, val in enumerate(file.readlines()):
-        print(val)
         if line == 1:
             words = val
             break
@@ -21,8 +21,6 @@ def read(example):
         else:
             word += x
     list2[len(word)].append(word)
-    print(list2)
-
     count = 0
     length = 0
     add = ""
@@ -46,63 +44,42 @@ def read(example):
             elif x != "\n":
                 add += x
     list1.append((length, count, character, add))
-    print(list1)
-
     return list1, list2
 
 
 def solve(spaces, words):
     graph = defaultdict(list)
-    count_rest = 0
     res = [""]*len(spaces)
     rest = list()
     i = 0
-    word_list = []
+    pos_list = []
     for l, pos, c, a in spaces:
         if c == "":
             if len(words[l]) == 1:
-                res[i] = words[l][0]
+                res[i] = words[l][0]#next(iter(words[l]))
             else:
-                rest.append((l, i-count_rest))
-                count_rest += 1
+                rest.append((l, i))
         else:
-            w = ""
-            count = 0
+            pos_list.append(i)
             for word in words[l]:
                 if word[pos] == c:
-                    if count == 0:
-                        w = word
-                    else:
-                        word_list.append(word)
-                        graph[word].append(i)
-                        graph[i].append(word)
-                    count += 1
-            if count == 1:
-                res[i] = w
-            else:
-                word_list.append(w)
-                graph[w].append(i)
-                graph[i].append(w)
+                    graph[word].append(i)
+                    graph[i].append(word)
         i += 1
-    print(graph)
-    print(words)
-    print(word_list)
-    while word_list:
-
-        for word in word_list:
-            if len(graph[word]) == 1:
-                print(word)
-                print(graph[word])
-                res[graph[word][0]] = word
-                x = graph[word][0]
-                for s in graph[graph[word][0]]:
-                    print(graph[s])
+    while pos_list:
+        j = 0
+        for y in range(len(pos_list)):
+            i = pos_list[j]
+            if len(set(graph[i])) == 1:
+                res[i] = graph[i][0]
+                words[len(graph[i][0])].remove(graph[i][0])
+                x = graph[i][0]
+                for s in set(graph[graph[i][0]]):
                     graph[s].remove(x)
-                word_list.remove(word)
-    print(words)
-    print(rest)
+                pos_list.remove(i)
+            else:
+                j += 1
     for l, i in rest:
-        print(words[l][0])
         res[i] = words[l][0]
 
     return res
@@ -112,4 +89,5 @@ print('Geben Sie die Nummer eines Beispiels ein:')
 example = input()
 spaces, words = read(example)
 res = solve(spaces, words)
-print(res)
+for i, word in enumerate(res):
+    print(word + spaces[i][3], end=" ")
