251fe4446400ddedf6989b7fe924c4093e4f364c
TPVPfaller <48595504+TPVPfaller@users.noreply.github.com>
2020-11-20 16:18:46 +0100

    Less code

--- Bwinf39_R1/Aufgabe1.py
+++ Bwinf39_R1/Aufgabe1.py
@@ -2,92 +2,73 @@ from collections import defaultdict
 
 
 def read(example):
+    spaces = list()
+    words = defaultdict(list)
     file = open("raetsel" + example + ".txt", "r")
-    words = ""
-    spaces = ""
     for line, val in enumerate(file.readlines()):
-        if line == 1:
-            words = val
-            break
+        if line:
+            for word in val.split():
+                words[len(word)].append(word)
         else:
-            spaces = val
-
-    word = ""
-    list2 = defaultdict(list)
-    for x in words:
-        if x == " ":
-            list2[len(word)].append(word)
-            word = ""
-        else:
-            word += x
-    list2[len(word)].append(word)
-    count = 0
-    length = 0
-    add = ""
-    character = ""
-    list1 = list(tuple())
-    for x in spaces:
-        if x == " ":
-            list1.append((length, count, character, add))
-            count = 0
-            length = 0
-            character = ""
-            add = ""
-        elif x == "_":
-            length += 1
-            if character == "":
-                count += 1
-        else:
-            if character == "" and x not in {".", ",", "!", "?", "(", ")", ";", "-", "\n"}:
-                character = x
-                length += 1
-            elif x != "\n":
-                add += x
-    list1.append((length, count, character, add))
-    return list1, list2
+            length, pos = 0, 0
+            add, character = "", ""
+            for c in val:
+                if c == " ":
+                    spaces.append((length, pos, character, add))
+                    length, pos = 0, 0
+                    add, character = "", ""
+                elif c in {".", ",", "!", "?", ";"}:
+                    add = c
+                else:
+                    if c == "_":
+                        length += 1
+                        if not character:
+                            pos += 1
+                    elif c != "\n":
+                        character = c
+                        length += 1
+            spaces.append((length, pos, character, add))
+    return spaces, words
 
 
 def solve(spaces, words):
     graph = defaultdict(list)
     res = [""]*len(spaces)
-    rest = list()
-    i = 0
-    pos_list = []
-    for l, pos, c, a in spaces:
-        if c == "":
-            if len(words[l]) == 1:
-                res[i] = words[l][0]#next(iter(words[l]))
-            else:
-                rest.append((l, i))
+    rest, pos_list = list(), list()
+    for i, val in enumerate(spaces):
+        l, pos, c, a = val
+        if len(words[l]) == 1:
+            res[i] = words[l][0]
+        elif c == "":
+            rest.append((l, i))
         else:
             pos_list.append(i)
             for word in words[l]:
                 if word[pos] == c:
                     graph[word].append(i)
                     graph[i].append(word)
-        i += 1
+    print(graph)
     while pos_list:
         j = 0
-        for y in range(len(pos_list)):
-            i = pos_list[j]
-            if len(set(graph[i])) == 1:
-                res[i] = graph[i][0]
-                words[len(graph[i][0])].remove(graph[i][0])
-                x = graph[i][0]
-                for s in set(graph[graph[i][0]]):
+        for _ in range(len(pos_list)):
+            p = pos_list[j]
+            if len(set(graph[p])) == 1:
+                res[p] = graph[p][0]
+                words[len(graph[p][0])].remove(graph[p][0])
+                x = graph[p][0]
+                for s in set(graph[graph[p][0]]):
                     graph[s].remove(x)
-                pos_list.remove(i)
+                pos_list.remove(p)
             else:
                 j += 1
-    for l, i in rest:
-        res[i] = words[l][0]
-
+    for l, pos in rest:
+        res[pos] = words[l][0]
     return res
 
 
 print('Geben Sie die Nummer eines Beispiels ein:')
-example = input()
-spaces, words = read(example)
+spaces, words = read(input())
 res = solve(spaces, words)
-for i, word in enumerate(res):
-    print(word + spaces[i][3], end=" ")
+
+for position, word in enumerate(res):
+    print(word + spaces[position][3], end=" ")
