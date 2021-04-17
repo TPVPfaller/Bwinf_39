c27dd7276c887d73377df9c1a49aba1537901d40
TPVPfaller <48595504+TPVPfaller@users.noreply.github.com>
2021-04-18 01:40:20 +0200

    greedy algo working

--- Bwinf39_R2/Aufgabe1.py
+++ Bwinf39_R2/Aufgabe1.py
@@ -1,7 +1,9 @@
 from functools import wraps
 from collections import defaultdict
 import tkinter as tk
-import time
+from time import time
+from operator import add
+from functools import reduce
 
 
 def timing(f):
@@ -32,32 +34,104 @@ class Draw:
     def __init__(self):
         root = tk.Tk()
         root.title("Aufgabe1")
-        root.geometry("1600x400+0+0")
+        root.geometry("1520x320+0+0")
         self.color_index = 0
-        self.canvas = tk.Canvas(root, bg="white", height=110, width=1510)
-        self.canvas.create_rectangle(10, 10, 1500, 100)
+        self.canvas = tk.Canvas(root, bg="white", height=320, width=1520)
+        self.canvas.create_rectangle(10, 10, 1510, 210)
         self.canvas.pack()
 
     def add_rectangle(self, r):
-        colors = ['CadetBlue4', 'turquoise1', 'salmon', 'red', 'green', 'blue', 'cyan', 'yellow', 'magenta', 'ivory4',
-                  'VioletRed3', 'DarkOrange3', 'lawn green', 'chocolate2', 'purple2', 'coral1', 'firebrick3']
-        self.canvas.create_rectangle(r[0]*1.5+10, r[1]+10, (r[0]+r[2])*1.5+10, (r[1]+r[3])*10, fill=colors[self.color_index], width=0)
+        colors = ['CadetBlue4', 'sandy brown', 'red', 'green', 'blue', 'cyan', 'yellow', 'magenta', 'ivory4',
+                  'grey76', 'DarkOrange4', 'lawn green', 'purple2', 'coral1', 'firebrick4']
+        print(r)
+        print(colors[self.color_index] + ":", str(r[0]) + "m" + ", " + str(r[1]+8) + "-" + str(r[3]+r[1]+8) + " Uhr")
+        self.canvas.create_rectangle(r[0]*1.5+10, r[1]*20+10, (r[0]+r[2])*1.5+10, (r[1]+r[3])*20+10, fill=colors[self.color_index], width=0)
 
         if self.color_index == len(colors)-1:
             self.color_index = 0
         else:
             self.color_index += 1
 
-    def finish(self):
+    def finish(self, space):
+        self.canvas.create_text(150, 250, fill="black", font="Times 15 bold",
+                                text="Umsatz: "+str(space)+"€")
+        for i in range(1, 10):
+            self.canvas.create_line(10, 10+(20*i), 1510, 10+(20*i))
         tk.mainloop()
 
 
-# this algorithm defines the upper bound
-def greedy(queue):
+def close_gap(position, skyline, gap, points):
+    if position[1] == 0:
+        lowest = skyline[gap]
+    else:
+        if position[1] == 9:
+            skyline[9] = skyline[8]
+            points[9] = [skyline[8], 9]
+            return
+        if skyline[position[1]] == skyline[position[1]-1]:
+            skyline[position[1]] = skyline[gap]
+            points[position[1]] = [skyline[gap], position[1]]
+            return
+        lowest = min(skyline[position[1] - 1], skyline[gap])
+    for i in range(position[1], gap):
+        skyline[i] = lowest
+        points[i] = [skyline[i], i]
+    return
+
+
+def feasible_rectangle(points, queue, skyline, feasible_position=0):
+    if feasible_position == 10:
+        return 0, 0
+    position = 0
+    p = points.copy()
+    p.sort()
+    for e in p:
+        if len(queue[e[1]]) != 0:
+            if e[1] >= feasible_position:
+                position = e
+                break
+        else:
+            for s in range(e[1]+1, 10):
+                if skyline[s] > e[0]:
+                    close_gap(e, skyline, s, points)
+                    break
+    if position == 0:
+        return 0, 0
+    count = 0
+    for r in queue[position[1]]:
+        gap = 0
+        for e in range(r[3] + 1, r[4]):
+            if skyline[e] > position[0]:
+                gap = e  # right side of gap
+                break
+        if not gap:
+            if position[0] + r[2] > 1000:
+                count += 1
+                continue
+            queue[position[1]].remove(r)
+            return position, r
+    # if not all rectangles exceed the limit 1000
+    if count != len(queue[position[1]]):
+        close_gap(position, skyline, gap, points)
+
+    return feasible_rectangle(points, queue, skyline, feasible_position+1)
+
+
+def greedy(queue, skyline, res):
     space = 0
+    for k in queue:
+        queue[k].sort(reverse=True)
+    points = [[skyline[0], i] for i in range(10)]
 
-
-
+    while queue:
+        pos, rectangle = feasible_rectangle(points, queue, skyline)
+        if rectangle == 0:
+            return space
+        for i in range(rectangle[3], rectangle[4]):
+            skyline[i] = (pos[0] + rectangle[2])
+            points[i] = [skyline[i], i]
+        space += rectangle[0]
+        res.append([pos[0], pos[1], rectangle[2], rectangle[1]])
     return space
 
 
@@ -69,29 +143,34 @@ def solve(shops):
         shop = shops.pop(0)
         res.append([border, 0, shop[1], shop[0]])
         border += shop[1]
-    print(shops)
-
     skyline = [border] * 10
-    print(skyline)
     queue = defaultdict(list)
     for s in shops:
-        queue[s[2]].append(s)
-    print(queue)
-    upper_bound = greedy(queue)
-    # create candidate solutions
-    while queue:
-        queue
+        s.insert(0, s[0]*s[1])
+        queue[s[3]].append(s)
+    space = greedy(queue, skyline, res)
+    count = 1
+    print("Nicht angenommene Voranmeldungen: ")
+    for k in queue.values():
+        if k:
+            count = 0
+            print(k, end=" ")
+    if count:
+        print("/")
+    print()
+    return res, space
 
-    return res
 
-
-shops = read(1)
+print("Geben Sie hier die Nummer des Beispiels ein (1-7):")
+choice = input()
+shops = read(choice)
 shops.sort(reverse=True)    # decreasing order
-print(shops)
-result = solve(shops)
-print(result)
 draw = Draw()
+
+result, space = solve(shops)
+
 for r in result:
     draw.add_rectangle(r)
 
-draw.finish()
+print("Umsatz:", space)
+draw.finish(space)
